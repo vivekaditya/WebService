d3e9aa10c9c620cbad132849e9b6c8ce846f8482
Vivekaditya <vivek.aditya@abzooba.com>
2017-05-23 23:41:16 +0530

    First Commit

---
+++ server.py
@@ -0,0 +1,43 @@
+import os
+# We'll render HTML templates and access data sent by POST
+# using the request object from flask. Redirect and url_for
+# will be used to redirect the user once the upload is done
+# and send_from_directory will help us to send/show on the
+# browser the file that the user just uploaded
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+from PIL import Image
+from io import BytesIO
+import base64
+import datetime
+import time
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/image')
+def image():
+    ts = time.time()
+    image = request.args.get('img', '''R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==''' )
+    im = Image.open(BytesIO(base64.b64decode(image)))
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    im.convert('RGB').save(os.path.join(app.config['UPLOAD_FOLDER'],st+".jpg"))
+    return "works"
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=int("85"),
+        debug=True
+    )
